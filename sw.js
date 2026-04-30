d5cc85ade9ea5bdb93b5ce4d06324381e3c0d544
Wellington Domiciano <wldomiciano@gmail.com>
2026-04-30 15:35:29 -0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "calculo-de-producao-cache-v1";
+const URLS = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
